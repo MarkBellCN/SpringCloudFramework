45b093290728cce5ee6fe67eb486def5e0eec0cf
马旭强 <markbell_cn@gmail.com>
2019-11-27 08:42:14 +0800

    修改swagger

--- app-server/sm-server/src/main/java/com/hollysys/iods/web/api/SmServerApplication.java
+++ app-server/sm-server/src/main/java/com/hollysys/iods/web/api/SmServerApplication.java
@@ -1,12 +1,10 @@
 package com.hollysys.iods.web.api;
 
-import com.hollysys.platform.common.web.annotations.EnableWebMvcAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableWebMvcAdapter
 @EnableDiscoveryClient
 public class SmServerApplication {
     public static void main(String[] args) {
