0b5ec4f7c5436819c5df3e01b8d2e3445d6cd2aa
马旭强 <markbell_cn@gmail.com>
2019-12-17 16:22:41 +0800

    修改错误码

--- auth/auth-server/src/main/java/com/hollysys/platform/auth/server/exception/AuthErrorType.java
+++ auth/auth-server/src/main/java/com/hollysys/platform/auth/server/exception/AuthErrorType.java
@@ -7,21 +7,21 @@ import lombok.Getter;
 
 @Getter
 public enum AuthErrorType implements ErrorType {
-    INVALID_REQUEST("00020001","invalid.request"),
-    INVALID_CLIENT("00020002","invalid.client"),
-    INVALID_GRANT("00020003","invalid.grant"),
-    INVALID_SCOPE("00020004","invalid.scope"),
-    INVALID_TOKEN("00020005","invalid.token"),
-    INSUFFICIENT_SCOPE("00020006","insufficient.scope"),
-    REDIRECT_URI_MISMATCH("00020007","redirect.uri.mismatch"),
-    ACCESS_DENIED("00020008","access.denied"),
-    METHOD_NOT_ALLOWED("00020009","method.not.allowed"),
-    SERVER_ERROR("00020010","server.error"),
-    UNAUTHORIZED_CLIENT("00020011","unauthorized.client"),
-    UNAUTHORIZED("00020012","unauthorized"),
-    UNSUPPORTED_RESPONSE_TYPE("00020013","unsupported.response.type"),
-    UNSUPPORTED_GRANT_TYPE("00020014","unsupported.grant.type"),
-    USERNAME_NOT_FOUND("00020015","username.not.found"),
+    INVALID_REQUEST("30020001","invalid.request"),
+    INVALID_CLIENT("30020002","invalid.client"),
+    INVALID_GRANT("30020003","invalid.grant"),
+    INVALID_SCOPE("30020004","invalid.scope"),
+    INVALID_TOKEN("30020005","invalid.token"),
+    INSUFFICIENT_SCOPE("30020006","insufficient.scope"),
+    REDIRECT_URI_MISMATCH("30020007","redirect.uri.mismatch"),
+    ACCESS_DENIED("30020008","access.denied"),
+    METHOD_NOT_ALLOWED("30020009","method.not.allowed"),
+    SERVER_ERROR("30020010","server.error"),
+    UNAUTHORIZED_CLIENT("30020011","unauthorized.client"),
+    UNAUTHORIZED("30020012","unauthorized"),
+    UNSUPPORTED_RESPONSE_TYPE("30020013","unsupported.response.type"),
+    UNSUPPORTED_GRANT_TYPE("30020014","unsupported.grant.type"),
+    USERNAME_NOT_FOUND("30020015","username.not.found"),
     ;
 
     /**
--- common/core/src/main/java/com/hollysys/platform/common/core/exception/SystemErrorType.java
+++ common/core/src/main/java/com/hollysys/platform/common/core/exception/SystemErrorType.java
@@ -4,15 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum SystemErrorType implements ErrorType {
-    SYSTEM_ERROR("00000000","system.error"),
-    SYSTEM_BUSY("00000001","system.busy"),
+    SYSTEM_ERROR("10000000","system.error"),
+    SYSTEM_BUSY("10000001","system.busy"),
 
-    ARGUMENT_NOT_VALID("00000101","argument.not.valid"),
-    UPLOAD_FILE_SIZE_LIMIT("00000102","upload.file.size.limit"),
+    ARGUMENT_NOT_VALID("10000101","argument.not.valid"),
+    UPLOAD_FILE_SIZE_LIMIT("10000102","upload.file.size.limit"),
 
-    GATEWAY_NOT_FOUND_SERVICE("00010001","gateway.not.found.service"),
-    GATEWAY_ERROR("00010002","gateway.error"),
-    GATEWAY_CONNECT_TIME_OUT("00010003","gateway.connect.time.out");
+    GATEWAY_NOT_FOUND_SERVICE("10010001","gateway.not.found.service"),
+    GATEWAY_ERROR("10010002","gateway.error"),
+    GATEWAY_CONNECT_TIME_OUT("10010003","gateway.connect.time.out");
 
 
 
